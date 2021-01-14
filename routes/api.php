0628dd4ca013b51381328ab6e87b3155c56f1fcd
takoage <dima30051999@mail.ru>
2021-01-14 22:27:46 +0700

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\SubtaskController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -40,10 +41,11 @@ use Illuminate\Support\Facades\Route;
         Route::post('/getOne', [ProjectController::class, 'showOne']);
         Route::patch('/', [TaskController::class, 'update']);
         Route::delete('/', [TaskController::class, 'destroy']);});
-//            Route::prefix('/subtasks/{subtask}')->group(function () {
-//                Route::get('', 'SubtaskController@show');
-//                Route::patch('/', 'SubtaskController@update');
-//                Route::delete('/', 'SubtaskController@destroy');
+    Route::prefix('/subtasks')->group(function () {
+        Route::post('/', [SubtaskController::class, 'store']);
+        //Route::post('/{subtask}', [SubtaskController::class, 'show']);
+        Route::patch('/', 'SubtaskController@update');
+        Route::delete('/', 'SubtaskController@destroy');});
 //                Route::prefix('/attachments/{attachment}')->group(function () {
 //                    Route::get('', 'AttachmentController@show');
 //                    Route::patch('/', 'AttachmentController@update');
