2bd3321e1ae00b2f3d7e8a17ae2b7c5bfe618620
takoage <dima30051999@mail.ru>
2021-01-14 20:58:38 +0700

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
